dbfd029183728b17c6cbdc19e4a7b5494b088ae4
Michael <michael.peshin@gmail.com>
2024-08-28 11:05:15 +0400

    Исправлен баг и добавлено свойство id

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,7 +13,7 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
@@ -24,11 +24,12 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'This character do not have description',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
         }
     }
 }
