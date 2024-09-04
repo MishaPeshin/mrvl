561a659ebbe7a4dd7fee90d3c86b078f01087d7a
Michael <michael.peshin@gmail.com>
2024-09-04 15:58:00 +0400

    Добавлен компонент поиска персонажа по имени

---
+++ src/components/charSearchForm/CharSearchForm.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+import { Formik, Form, Field, ErrorMessage as FormikErrorMessage } from 'formik';
+import * as yup from 'yup';
+import { Link } from 'react-router-dom';
+
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import useMarvelService from '../../services/MarvelService';
+
+import './charSearchForm.scss';
+
+const CharSearchForm = () => {
+    const [char, setChar] = useState(null);
+    const { error, loading, getCharacterByName, clearError } = useMarvelService();
+
+    const onCharLoaded = (char) => {
+        setChar(char);
+    }
+
+
+    const updateChar = (name) => {
+        clearError();
+
+        getCharacterByName(name)
+            .then(onCharLoaded)
+    }
+
+    const errorMessage = error ? <div className="error"><ErrorMessage /></div> : null;
+    const result = !char ? null : char.length > 0 ?
+        <div className="char__wrapper">
+            <div className="char__message char__message_success">There is! Visit {char[0].name} page?</div>
+            <Link to={`/characters/${char[0].id}`} className='button button__secondary'>
+                <div className='inner'>To page</div>
+            </Link>
+        </div> :
+        <div className="char__message char__message_error">
+            The character was not found. Check the name and try again
+        </div>
+
+    return (
+        <div className="char__search">
+            <Formik
+                initialValues={{ charName: '' }}
+                validationSchema={yup.object({
+                    charName: yup.string().required('This field is required')
+                })}
+                onSubmit={({ charName }) => {
+                    updateChar(charName)
+                }}
+            >
+                <Form className='char__form'>
+                    <label className='char__title' htmlFor='charName'>Or find a character by name:</label>
+                    <div className="char__wrapper">
+                        <Field
+                            className='char__field'
+                            placeholder='Enter name'
+                            type="text"
+                            id="charName"
+                            name="charName" />
+                        <button
+                            type="submit"
+                            className='button button__main'
+                            disabled={loading}>
+                            <div className='inner'>FIND</div>
+                        </button>
+                    </div>
+                    <FormikErrorMessage className="char__message char__message_error" name="charName" component="div" />
+                </Form>
+            </Formik>
+            {errorMessage}
+            {result}
+        </div>
+    )
+}
+
+export default CharSearchForm;
